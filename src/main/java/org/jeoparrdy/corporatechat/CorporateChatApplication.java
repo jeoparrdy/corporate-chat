9c24daa0cd04613426753ede25dc8a27f773e0a1
jeoparrdy <jeoparrdy@gmail.com>
2021-05-24 13:43:26 +0300

    initial commit

---
+++ src/main/java/org/jeoparrdy/corporatechat/CorporateChatApplication.java
@@ -0,0 +1,13 @@
+package org.jeoparrdy.corporatechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorporateChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jeoparrdy/corporatechat/CorporateChatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jeoparrdy.corporatechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporateChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
